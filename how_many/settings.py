412cb52116a1431db291619640b26a7adc7d332f
Marcos Raulino <marcosfsraulino@gmail.com>
2023-08-11 02:48:29 -0300

    fix: fix bad request (400) error by adding howmanymtg.onrender.com to ALLOWED_HOSTS

--- how_many/settings.py
+++ how_many/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['how-many.onrender.com', 'howmanymtg.com', 'www.howmanymtg.com']
+ALLOWED_HOSTS = ['howmanymtg.onrender.com', 'howmanymtg.com', 'www.howmanymtg.com']
 
 
 # Application definition
