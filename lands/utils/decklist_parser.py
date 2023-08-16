470d654df78f52c1bb700b5354d69d3cc7cb6f29
Marcos Raulino <marcosfsraulino@gmail.com>
2023-08-16 17:19:34 -0300

    chore: adds decklist format check for input

--- lands/utils/decklist_parser.py
+++ lands/utils/decklist_parser.py
@@ -37,39 +37,83 @@ class DecklistParser:
         Returns:
             dict: A dictionary containing the parsed decklist data.
         """
-        lines = self._decklist.split('\n')
+        lines = self._decklist.split('\r\n')
 
-        card_pattern = r'^\s*(?P<quantity>\d+)\s+(?P<card_name>[\w\s\'\,\-]+)'
-        set_pattern = r'\((?P<set_code>\w+)\)\s+(?P<set_number>\d+)'
+        if self._is_valid_decklist(lines):
+            card_pattern = r'^\s*(?P<quantity>\d+)\s+(?P<card_name>[\w\s\'\,\-]+)'
+            set_pattern = r'\((?P<set_code>\w+)\)\s+(?P<card_set_id>\d+)'
 
-        current_section = None
+            current_section = None
 
-        for line in lines:
-            if line.startswith('companion'):
-                current_section = line.strip()
-            elif line.startswith('deck'):
-                current_section = line.strip()
-            elif line.startswith('sideboard'):
-                current_section = line.strip()
-            else:
-                match = re.search(card_pattern, line)
-                if match:
-                    quantity = int(match.group('quantity'))
-                    card_name = match.group('card_name').strip()
-                    set_info = re.search(set_pattern, line)
+            for line in lines:
+                if line.startswith('companion'):
+                    current_section = line.strip()
+                elif line.startswith('deck'):
+                    current_section = line.strip()
+                elif line.startswith('sideboard'):
+                    current_section = line.strip()
+                else:
+                    match = re.search(card_pattern, line)
+                    if match:
+                        quantity = int(match.group('quantity'))
+                        card_name = match.group('card_name').strip()
+                        set_info = re.search(set_pattern, line)
 
-                    if set_info:
-                        set_code = set_info.group('set_code')
-                        set_number = set_info.group('set_number')
-                    else:
-                        set_code = None
-                        set_number = None
-                    
-                    self._parsed_decklist[current_section].setdefault(card_name, [])
-                    self._parsed_decklist[current_section][card_name].append({
-                        'quantity': quantity,
-                        'set_code': set_code,
-                        'set_number': set_number,
-                    })
+                        if set_info:
+                            set_code = set_info.group('set_code')
+                            card_set_id = set_info.group('card_set_id')
+                        else:
+                            set_code = None
+                            card_set_id = None
 
-        return self._parsed_decklist
+                        self._parsed_decklist[current_section].setdefault(
+                            card_name, []
+                        )
+                        self._parsed_decklist[current_section][card_name].append(
+                            {
+                                'quantity': quantity,
+                                'set_code': set_code,
+                                'card_set_id': card_set_id,
+                            }
+                        )
+
+            return self._parsed_decklist
+        
+        else:
+            raise KeyError('Invalid entry. Enter a valid decklist by explicitly entering each section that your decklist has. Example: Companion ... Deck ... Sideboard ...')
+    
+
+    def _is_valid_decklist(self, decklist):
+        if self._is_valid_decklist_start(decklist):
+            if self._is_valid_decklist_end(decklist):
+                return True
+        return False
+    
+    def _is_valid_decklist_start(self, decklist):
+        if 'companion' in decklist[0]:
+            if 'deck' in decklist[3]:
+                return True
+        
+        elif 'deck' in decklist[0]:
+                return True
+
+    def _is_valid_decklist_end(self, decklist):
+        deck_index = decklist.index('deck')
+        sublist_after_deck = decklist[deck_index + 1 :]
+        sublist_after_deck_len = len(sublist_after_deck)
+        if '' in sublist_after_deck:
+            if 'sideboard' in sublist_after_deck:
+                if (
+                    sublist_after_deck.index('')
+                    == sublist_after_deck.index('sideboard') - 1
+                ):
+                    return True
+
+            elif (
+                sublist_after_deck.index('') + 1
+                == sublist_after_deck_len
+            ):
+                return True
+
+        elif 'sideboard' not in sublist_after_deck:
+            return True
