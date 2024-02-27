082fbde5c430c1e2238c95aec4c42a07219cbe6b
Marcos Raulino <marcosfsraulino@gmail.com>
2024-02-27 16:17:49 -0300

    docs(swagger): add swagger.py for api documentation
    - Create swagger.py with configurations for generation APIP documentation using drf_yasg

---
+++ api/swagger.py
@@ -0,0 +1,34 @@
+from django.urls import path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='How Many',
+        default_version='0.1.0',
+        description='Easy-to-use tool to build decks with a more consistent mana base',
+        license=openapi.License(name='GPL-3.0-only'),
+    ),
+    public=True,
+    permission_classes=(permissions.IsAuthenticatedOrReadOnly,),
+)
+
+urlpatterns = [
+    re_path(
+        r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0),
+        name='schema-json',
+    ),
+    path(
+        'swagger/',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui',
+    ),
+    path(
+        'redoc/',
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc',
+    ),
+]
