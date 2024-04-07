9f15f757b95742553dab7e1e7e0840008da5570d
Marcos Raulino <marcosfsraulino@gmail.com>
2024-04-07 17:04:26 -0300

    feat(admin): update admin configuration for new models
    - Replace existing admin classes with new admin classes for CardModel, DeckModel, and CardDeckModel
    - Add new admin classes: CardModelAdmin for CardModel, DeckModelAdmin for DeckModel, DeckCardModelAdmin for CardDeckModel

--- api/admin.py
+++ api/admin.py
@@ -1,46 +1,82 @@
 from django.contrib import admin
 
-from api.models import (
-    Card,
-    Commander,
-    Companion,
-    Decklist,
-    DecklistCard,
-    Maindeck,
-    Sideboard,
-)
+from api.models import CardDeckModel, CardModel, DeckModel
 
 
-@admin.register(Card)
-class CardAdmin(admin.ModelAdmin):
-    list_display = ['name']
+@admin.register(CardModel)
+class CardModelAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'mana_cost',
+        'cmc',
+        'type_line',
+        'rarity',
+        'oracle_text',
+        'is_land',
+        'is_cheap_card_draw_spell',
+        'is_cheap_mana_ramp_spell',
+        'is_land_spell_mdfc',
+    )
+    list_display_links = ('id', 'name')
+    search_fields = (
+        'name',
+        'mana_cost',
+        'type_line',
+        'oracle_text',
+    )
+    list_filter = (
+        'cmc',
+        'rarity',
+        'is_land',
+        'is_cheap_card_draw_spell',
+        'is_cheap_mana_ramp_spell',
+        'is_land_spell_mdfc',
+    )
+    ordering = ('name',)
+    list_per_page = 20
 
 
-@admin.register(DecklistCard)
-class DecklistCardAdmin(admin.ModelAdmin):
-    list_display = ['card', 'quantity']
+@admin.register(DeckModel)
+class DeckModelAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'created_at',
+        'is_commander_deck',
+        'has_companion',
+        'maindeck_card_count',
+        'non_land_card_count',
+        'non_land_cmcs_count',
+        'cheap_card_draw_spell_count',
+        'cheap_mana_ramp_spell_count',
+        'non_mythic_land_spell_mdfc_count',
+        'mythic_land_spell_mdfc_count',
+        'average_cmc',
+        'recommended_number_of_lands',
+    )
+    list_display_links = ('id', 'created_at')
+    search_fields = (
+        'created_at',
+        'maindeck_card_count',
+        'non_land_card_count',
+        'non_land_cmcs_count',
+        'cheap_card_draw_spell_count',
+        'cheap_mana_ramp_spell_count',
+        'non_mythic_land_spell_mdfc_count',
+        'mythic_land_spell_mdfc_count',
+        'average_cmc',
+        'recommended_number_of_lands',
+    )
+    list_filter = ('is_commander_deck', 'has_companion')
+    ordering = ('created_at',)
+    list_per_page = 20
 
 
-@admin.register(Commander)
-class CommanderAdmin(admin.ModelAdmin):
-    filter_horizontal = ['commanders_cards']
-
-
-@admin.register(Companion)
-class CompanionAdmin(admin.ModelAdmin):
-    list_display = ['companion_card']
-
-
-@admin.register(Maindeck)
-class MaindeckAdmin(admin.ModelAdmin):
-    filter_horizontal = ['maindeck_cards']
-
-
-@admin.register(Sideboard)
-class SideboardAdmin(admin.ModelAdmin):
-    filter_horizontal = ['sideboard_cards']
-
-
-@admin.register(Decklist)
-class DecklistAdmin(admin.ModelAdmin):
-    list_display = ['commanders', 'companion', 'maindeck', 'sideboard']
+@admin.register(CardDeckModel)
+class DeckCardModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'deck', 'card', 'quantity', 'section')
+    list_display_links = ('id',)
+    search_fields = ('card__name',)
+    list_filter = ('section',)
+    ordering = ('id',)
+    list_per_page = 40
