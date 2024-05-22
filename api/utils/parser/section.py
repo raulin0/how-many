0ce6b6c17339f01944f526b71aa39fe9d59e5b43
Marcos Raulino <marcosfsraulino@gmail.com>
2024-05-22 16:57:36 -0300

    docs: add comprehensive docstrings to Section class and its methods

--- api/utils/parser/section.py
+++ api/utils/parser/section.py
@@ -2,14 +2,35 @@ from api.utils.parser.card import Card
 
 
 class Section:
+    """
+    Represents a section that contains a set of cards.
+
+    Attributes:
+        _name (str): The name of the section.
+        _cards (list[Card]): The list of cards contained in the section.
+    """
+
     def __init__(self, name: str, cards: list[Card]):
+        """
+        Initializes a new instance of the Section class.
+
+        Parameters:
+            name (str): The name of the section.
+            cards (list[Card]): The list of cards contained in the section.
+        """
         self._name = name
         self._cards = cards
 
     @property
     def name(self):
+        """
+        str: Returns the name of the section.
+        """
         return self._name
 
     @property
     def cards(self):
+        """
+        list[Card]: Returns the list of cards contained in the section.
+        """
         return self._cards
